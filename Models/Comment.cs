bc0e3ee2e2719c4822330075ec1031ad53a3cb08
Yahya TAJANY <ytajany@mediaocean.com>
2021-03-11 15:45:09 +0100

    Yahya

---
+++ Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demandeAdmin.Models
+{
+    public class Comment
+    {
+        public int id { get; set; }
+
+        public string body { get; set; }
+
+        public DateTime dateComment { get; set; }
+    }
+}
---
+++ Models/Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demandeAdmin.Models
+{
+    public class Document
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public string description { get; set; }
+
+    }
+}
