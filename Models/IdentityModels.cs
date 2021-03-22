e61db855685187cbb3a67c3542886e624b0aa7df
abdelwahed <anaitmessaoud@mbs-maroc.net>
2021-03-22 10:30:57 +0100

    Auth steps

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace demandeAdmin.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DPAContextModels", throwIfV1Schema: false)
         {
         }
 
